cd627938909bd814c3a0aa198fe3afc1d3db2a73
nikkuts <nickkuts888@gmail.com>
2023-02-12 19:41:35 +0200

    завдання 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('ul#ingredients');
+// const listIngredients = [];
+
+// ingredients.forEach((ingredient) => {
+//   const individualIngredient = document.createElement("li");
+//   individualIngredient.textContent = ingredient;
+//   individualIngredient.class = "item";
+//   listIngredients.push(individualIngredient);
+// });
+
+// list.after(...listIngredients);
+
+const markup = ingredients.map((ingredient) => 
+  `<li class="item">${ingredient}</li>`).join("");
+
+list.insertAdjacentHTML("afterend", markup);
