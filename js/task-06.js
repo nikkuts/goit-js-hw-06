cae4b19db8f59f41c1685c447bd9f5358e698695
nikkuts <nickkuts888@gmail.com>
2023-02-19 00:21:40 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,8 +2,10 @@ const textInputEl = document.querySelector('input[data-length="6"]');
 
 const onCheckNumberSymbols = (event) => { 
     if(event.currentTarget.value.length === Number(textInputEl.dataset.length)) {
+        textInputEl.classList.remove("invalid");
         textInputEl.classList.add("valid");
     } else {
+        textInputEl.classList.remove("valid");
         textInputEl.classList.add("invalid");
     }
 };
