9c3b72b136b4d75aa6b72feffb622504812c8700
nikkuts <nickkuts888@gmail.com>
2023-02-18 20:54:51 +0200

    завдання 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.body.querySelector('.login-form');
+// const emailEl = formEl.querySelector('input[name="email"]');
+// const passwordEl = formEl.querySelector('input[name="password"]');
 
+formEl.addEventListener("submit", onProcessesForm);
+
+function onProcessesForm (event) {
+    event.preventDefault();
+    const {email, password} = event.currentTarget.elements;
+
+    if (email.value === "" || password.value === "") {
+        alert('Всі поля повинні бути заповнені')
+    } else {
+        const result = {
+            email: email.value,
+            password: password.value,
+        };
+        console.log(result);
+        event.currentTarget.reset();
+    }
+};
