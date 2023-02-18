42d7f7eaf12ea5cccca8e845b9b550cbd0c06c09
nikkuts <nickkuts888@gmail.com>
2023-02-19 00:01:11 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,13 +1,13 @@
 const textInputEl = document.querySelector('input#name-input');
 const outputEl = document.querySelector('span#name-output');
 
-const handlerTextInput = (event) => {
-    outputEl.textContent = event.currentTarget.value;
+const onTextInput = (event) => {
+    if (event.currentTarget.value === "") {
+        outputEl.textContent = 'Anonymous';
+    } else {
+        outputEl.textContent = event.currentTarget.value;
+    }
 };
 
-textInputEl.addEventListener("input", handlerTextInput);
+textInputEl.addEventListener("input", onTextInput);
 
-
-// textInputEl.addEventListener("blur", () => {
-//     outputEl.textContent = 'Anonymous';
-// });
