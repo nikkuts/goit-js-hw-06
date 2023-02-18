551648a879165010e1f1b0c5fd79745e47ee4da8
nikkuts <nickkuts888@gmail.com>
2023-02-18 21:23:00 +0200

    завдання  9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonChangeColorEl = document.body.querySelector('button.change-color');
+const valueColorEl = document.body.querySelector('span.color');
+
+buttonChangeColorEl.addEventListener("click", onСhangesСolor);
+
+function onСhangesСolor (event) {
+  const randomColor = getRandomHexColor();  
+  document.body.style.backgroundColor = randomColor;
+    valueColorEl.textContent = randomColor;
+}
